be6100586f3098f01c1768b91d7cbbdbeb4b6c17
Mayurraj Singh Rathore <mayurraj.singh@webcluesinfotech.com>
2026-06-29 16:44:02 +0530

    add

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const letstalkbtn = document.querySelector('.let');
-const getstartbtn = document.querySelectorAll('.get');
+const getInTouchButton = document.querySelector('.get');
+const learnMoreButtons = document.querySelectorAll('.button');
 const contactSection = document.querySelector('#contact');
 const form = document.querySelector('.input');
 const darkModeButton = document.querySelector('.theme-toggle');
 
-if (getstartbtn && contactSection) {
-getstartbtn.addEventListener('click', () => {
+if (letstalkbutton && contactSection) {
+	getInTouchButton.addEventListener('click', () => {
 		contactSection.scrollIntoView({ behavior: 'smooth' });
 	});
 }
@@ -16,6 +16,16 @@ learnMoreButtons.forEach((button) => {
 	});
 });
 
+if (form) {
+	form.addEventListener('submit', (event) => {
+		event.preventDefault();
+		alert('Thanks for your message! We will contact you soon.');
+		form.reset();
+	});
+}
 
-
-
+if (darkModeButton) {
+	darkModeButton.addEventListener('click', () => {
+		document.body.classList.toggle('dark-mode');
+	});
+}
